ddd74ac805d5d02761a3e3ecaabe6a82d817d3f7
tienanh <tienanh12108991@gmail.com>
2020-07-05 16:11:20 +0700

    ++customer

--- app/Borrow.php
+++ app/Borrow.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Borrow extends Model
 {
-    //
+    public function Customer() {
+        return $this->belongsTo('App\Customer','customer_id');
+    }
 }
